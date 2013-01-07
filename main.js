0581189e075f5c25cd333f39d2c06e04a8c5f6ce
Jonah Meijers <jonah@Jonah-Swiffer.(none)>
2013-01-07 16:02:50 +0100

    License added

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+/*
+ * This file is part of the Muze Server-sent eventbus
+ *
+ * (c) Muze <info@muze.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 var http = require('http');
 var https = require('https');
 var sys = require('util');
